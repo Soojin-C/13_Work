061c06622dd45a42bc5686a53ea1761d85f71783
Soojin-C <schoi4@stuy.edu>
2018-11-08 11:02:39 -0500

    k13

---
+++ work13.c
@@ -0,0 +1,32 @@
+//Soojin Choi
+//Work 13: Fire up the bat signal
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static void sighandler(int signo)
+
+int main(){
+
+  while (1){
+
+    printf("PID: %d\n", getpid());
+    sleep(1);
+
+    (SIGINT, sighandler){
+
+      fd = open("text.txt", )
+
+    }
+
+    (SIGUSR1, sighandler){
+
+      printf("Parent PID: %d\n", );
+
+    }
+
+  }
+
+}
