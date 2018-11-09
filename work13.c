5c1713b19380fd75c015c429b2da9ede6484e66a
Soojin-C <schoi4@stuy.edu>
2018-11-09 13:28:58 -0500

    k13

--- work13.c
+++ work13.c
@@ -5,27 +5,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
 
-static void sighandler(int signo)
+static void sighandler(int signo){
+
+  if (signo == SIGINT){
+
+    int fd = open("text.txt", O_CREAT | O_WRONLY, 0777);
+    close(fd);
+    
+  }
+
+  if (signo == SIGUSR1){
+
+    printf("caught error\n");
+    
+  }
+
+};
 
 int main(){
 
   while (1){
 
+    signal(SIGINT, sighandler);
+
+    signal(SIGUSR1, sighandler);
+
     printf("PID: %d\n", getpid());
     sleep(1);
-
-    (SIGINT, sighandler){
-
-      fd = open("text.txt", )
-
-    }
-
-    (SIGUSR1, sighandler){
-
-      printf("Parent PID: %d\n", );
-
-    }
 
   }
 
